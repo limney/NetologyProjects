3d0a0b9f6905973a6f9eafbca0f91d75836c30ae
Dima <dv.klimov@dr-analitics.ru>
2016-11-30 17:31:22 +0300

    homework

--- ListOfCountries/Start.py
+++ ListOfCountries/Start.py
@@ -37,30 +37,52 @@ countries["Poland"] = {'sea': False, 'schengen': True, 'average_temperature': 15
 countries["Spain"] = {'sea': True, 'schengen': True, 'average_temperature': 25, 'currency_rate': 2, 'cost_day': 220}
 countries["China"] = {'sea': True, 'schengen': False, 'average_temperature': 10, 'currency_rate': 1.1, 'cost_day': 110}
 
-# -------------------------- JSON ---------------------------------------------------
+# -------------------------- JSON (запись) ---------------------------------------------------
 # Сохраним список стран в файл JSON
 with open('list_of_countries.json', 'w', encoding="UTF-8") as f:
     json.dump(countries, f)
 # Прочитаем список стар из файла в формате JSON
 with open('list_of_countries.json', 'r', encoding="UTF-8") as f:
     list_of_countries = json.load(f)
-pprint(list_of_countries)  # красиво напечатеам список стран
-
-# -------------------------- XML ----------------------------------------------------
-# Создадим XML документ
-root = ET.Element('countries')
-for country, properties in countries.items():
-    country_element = ET.SubElement(root, 'country')
-    country_element.text = country
-    for property_name, property_value in properties.items():
-        c = ET.SubElement(country_element, property_name)
-        c.text = str(property_value)
-# Сохраним список стран в файл XML
-with open('list_of_countries.xml', 'w') as f:
-    f.write("<?xml version='1.0'?>\n")  # заголовок
-    f.write(str(ET.dump(root)))
-
 #pprint(list_of_countries)  # красиво напечатеам список стран
+
+# -------------------------- XML (запись) ----------------------------------------------------
+# Создадим XML документ
+root = ET.Element("countries")
+for country, properties in countries.items():
+    country_element = ET.SubElement(root, "country", attrib={'name': country})
+    for propety_name, property_value in properties.items():
+        country_sea = ET.SubElement(country_element, propety_name)
+        country_sea.text = str(property_value)
+tree_countries = ET.ElementTree(root)  # дерево документа
+# Запишем список стран в файл XML
+tree_countries.write('list_of_countries.xml', encoding="UTF-8", xml_declaration=True)
+#print(ET.dump(tree_countries))  # красиво напечатеам список стран
+
+
+select = int(input("Нажмите - 1 чтобы загрузить списко стран из файла формата Json или 2 - чтобы загрузить из файла формата XML:\n"))
+
+if select == 1:
+    countries = {}
+    # ---------- JSON (чтение) ---------------------------
+    with open('list_of_countries.json') as f:
+        countries = json.load(f)
+        for country, properties in countries.items():
+            countries[country] = properties
+        # pprint(countries)
+elif select == 2:
+    countries = {}
+    # ---------- JSON (чтение) ---------------------------
+    convertor = {'cost_day': int, 'schengen': bool, 'average_temperature': int, 'sea': bool, 'currency_rate': float}
+    tree = ET.parse('list_of_countries.xml')
+    root = tree.getroot()
+    for country in root.findall("./country"):
+        properties = {}
+        for property in country:
+            properties[property.tag] = convertor[property.tag](property.text)
+        countries[country.get('name')] = properties
+else:
+    print("Вы введи не коректные данные, списки будут использованы из кода программы")
 
 print("_"*60)
 
