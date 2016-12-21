37d31c0aa78583a7e0cdccb22304cbb967c87180
Dima <dv.klimov@dr-analitics.ru>
2016-12-21 15:03:07 +0300

    home work VK

--- MyFriendsInVK/Start.py
+++ MyFriendsInVK/Start.py
@@ -4,38 +4,47 @@
 
 import networkx as nx
 import matplotlib.pyplot as plt
+from vk_client import UserVK
+import pickle
+from collections import Counter
+
+"""
+В файле vk_client.py я реализовал загрузки из ВК своих друзей и их друзей.
+Загрузка с ВК занимает много времени, поэтому я сохранил результат, и тут работаю с уже сохраненной версией моих друзей.
+"""
+
+my_friends = []
+# Читаем объект my_friends (JSON сериализация)
+with open('my_friends.pickle', 'rb') as f:
+    my_friends = pickle.load(f)
 
 
-ID_APP = 5786550
+# Найдем общих друзей среди моих друзей в ВК
+general_friends = Counter()
 
+for my_friend in my_friends:
+    general_friends[my_friend.my_name] += 1
+    for friend_my_friend in my_friend.friends:
+        general_friends[friend_my_friend.my_name] += 1
 
+print(" Общие друзья моих друзей в ВК:")
+count_general_friends = 0
+for friend, count in general_friends.items():
+    if count > 1:
+        print(friend, count)
+        count_general_friends += 1
+print("Общее количество общих друзей: %d" % count_general_friends)
 
-
-
-params = {
-    "access_token": KEY,
-    "v": VERSION
-}
-
-response = requests.get("https://api.vk.com/method/freinds.getOnline", params)
-
-for user_id in response.json()['response']:
-    response = requests.get("https://api.vk.com/method/users.get", {"user_id": user_id})
-    print(user_id)
-
-exit()
-
+# Сформируем граф моих друзей
 my_graph = nx.Graph()
+i_am = UserVK({"first_name": "Дмитрий", "last_name": "Климов"})  # это я
+my_graph.add_node(0, name=i_am.profile["last_name"])
 
+for index, my_friend in enumerate(my_friends):
+    my_graph.add_node(index+1, name=my_friend.profile["last_name"])
+    my_graph.add_edge(0, index+1)
 
-
-
-my_graph.add_node(1, name="Nick")
-my_graph.add_node(2, name="Jack")
-my_graph.add_edge(1, 2)
-
-nx.draw(my_graph)
+# Отрисуем граф
+nx.draw(my_graph, node_color='b')
 plt.show()
-
-# print(my_graph.)
 
--- MyFriendsInVK/vk_client.py
+++ MyFriendsInVK/vk_client.py
@@ -1,5 +1,25 @@
 import requests
 from urllib.parse import urlencode, urlparse
+import pickle
+
+class UserVK:
+    """
+    Пользователь в ВК
+    """
+    def __init__(self, profile):
+        self.friends = []
+        self.profile = profile
+
+    def __str__(self):
+        string_out = self.profile["first_name"] + " " + self.profile["last_name"]
+        for friend in self.friends:
+            string_out += "\n\t" + friend.profile["first_name"] + " " + friend.profile["last_name"]
+        return string_out
+
+    @property
+    def my_name(self):
+        return self.profile["first_name"] + " " + self.profile["last_name"]
+
 
 class ClientVK:
     """
@@ -20,17 +40,53 @@ class ClientVK:
         }
         token_url = "?".join((self.__AUTHORIZE, urlencode(params)))
 
-        token = "https://oauth.vk.com/blank.html#access_token=c93dfe19a243b84ead51d261b067b971e3e4fa7f599df7fcc85d62a57f21e1c2a1ffd4c8371d22dece23d&expires_in=86400&user_id=83492044"
-
         # token_url_response = requests.get(token_url)
-        print(token_url)
-        o = urlparse(token_url)
-        fragment = dict((i.splite("=") for i in o.fragment.splite("&")))
+        # print(token_url)
+
+        token_url_response = "https://oauth.vk.com/blank.html#access_token=c93dfe19a243b84ead51d261b067b971e3e4fa7f599df7fcc85d62a57f21e1c2a1ffd4c8371d22dece23d&expires_in=86400&user_id=83492044"
+
+        o = urlparse(token_url_response)
+        fragment = dict((i.split("=") for i in o.fragment.split("&")))
         return fragment["access_token"]
+
+    def get_my_friends(self):
+        access_token = self.get_access_token()
+        params = {
+            "access_token": access_token,
+            "v": self.__VERSION
+        }
+        id_users = requests.get("https://api.vk.com/method/friends.get", params)
+        my_friends = []
+        for user_id in id_users.json()["response"]["items"]:
+            user = requests.get("https://api.vk.com/method/users.get", {"user_id": user_id})
+            my_friends.append(UserVK(user.json()["response"][0]))
+            # Добавим друзей моего друга
+            try:
+                my_friend_id_users = requests.get("https://api.vk.com/method/friends.get", {"user_id": user_id})  # друзья друга
+                for my_friend_user_id in my_friend_id_users.json()["response"]:
+                    my_friend_user_id_user = requests.get("https://api.vk.com/method/users.get", {"user_id": my_friend_user_id})
+                    my_friends[-1].friends.append(UserVK(my_friend_user_id_user.json()["response"][0]))
+            except:
+                # Если по какой-то причине нельзя узнать друзей моего друга то пропускаем
+                pass
+        return my_friends
 
 
 if __name__ == "__main__":
 
     vk = ClientVK(app_id=5786550)
+    my_friends = vk.get_my_friends()
+    # for my_friend in my_friends:
+    #     print(my_friend)
 
-    print(vk.get_access_token())
+    # Сохраним объект my_friends (JSON сериализация)
+    with open('my_friends.pickle', 'wb') as f:
+        pickle.dump(my_friends, f)
+
+    # Читаем объект my_friends (JSON сериализация)
+    with open('my_friends.pickle', 'rb') as f:
+        my_friends_saved = pickle.load(f)
+
+    for my_friend in my_friends_saved:
+        print(my_friend)
+
